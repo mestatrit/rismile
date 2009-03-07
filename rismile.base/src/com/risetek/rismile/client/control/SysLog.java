ef8eba2a631e07c15f94d8879fed5bea05589191
ychun.w <ychun.w@gmail.com>
2009-03-07 14:07:57 +0000

    增加调试输出的框架

---
+++ rismile.base/src/com/risetek/rismile/client/control/SysLog.java
@@ -0,0 +1,13 @@
+/*
+ * 该类用来输出调试信息，也许我们应该在浏览器端开辟一个调试窗口。
+ * 当然我们也应该有办法关闭这些信息的输出。
+ * 
+ */
+package com.risetek.rismile.client.control;
+
+public class SysLog {
+	public static void log(String message)
+	{
+		
+	}
+}
