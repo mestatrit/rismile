5aeea6c07dd1a078d584b7d496779ed840f6045e
ychun.w <ychun.w@gmail.com>
2009-05-17 15:49:27 +0000

    正确实现图像数据的获取。

--- rismile.scada/src/com/risetek/scada/server/cameraPostServiceImpl.java
+++ rismile.scada/src/com/risetek/scada/server/cameraPostServiceImpl.java
@@ -3,6 +3,7 @@ package com.risetek.scada.server;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,8 +20,39 @@ public class cameraPostServiceImpl extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		GWT.log("camera", null);
-		byte[] img = (byte[])req.getAttribute("img");
-		ImageCache.imageCache.putImage(img);
+		int ContentLength = req.getContentLength();
+		GWT.log("content is: "+ContentLength, null);
+		byte[] cachebuf = null;
+		
+		InputStream imgData =  req.getInputStream();
+		try {
+			byte[] buf = new byte[10240];
+			int len;
+			while ((len = imgData.read(buf)) > 0) {
+				int oldlen;
+				if(cachebuf == null )
+					oldlen = 0;
+				else
+					oldlen = cachebuf.length;
+				
+				
+				byte[] newbuf = new byte[oldlen + len];
+				if( oldlen > 0)
+					System.arraycopy(cachebuf, 0, newbuf, 0, oldlen);
+				
+				System.arraycopy(buf, 0, newbuf, oldlen, len);
+				
+				cachebuf = newbuf;
+			}				
+			
+			imgData.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+		
+		ImageCache.imageCache.putImage(cachebuf);
 	}
 
 	@Override
