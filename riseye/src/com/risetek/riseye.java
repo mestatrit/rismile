aed047345f237b5a6950470c296a627bcc9cac89
ychun.w <ychun.w@gmail.com>
2009-05-17 15:45:11 +0000

    能够更换不同的图片

--- riseye/src/com/risetek/riseye.java
+++ riseye/src/com/risetek/riseye.java
@@ -1,6 +1,9 @@
 package com.risetek;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,6 +12,8 @@ public class riseye {
 
 	URL url = null;
 	String home = "http://riscada.appspot.com/scada/take";
+//	String home = "http://127.0.0.1:8080/scada/take";
+	static int fileNum = 2;
 	
 	public void PostIt()
 	{
@@ -20,7 +25,36 @@ public class riseye {
 			connector.setRequestProperty("Accept-Language", "zh-cn");
 			connector.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows 2000)");
 			connector.setDoOutput(true);
-			connector.setDoInput(true);			
+			connector.setDoInput(true);
+
+			try {
+				OutputStream out  = connector.getOutputStream();
+				if(++fileNum > 5 ) fileNum = 2;
+				String filename = "image/p"+fileNum+".jpg";
+				
+				System.out.println("POST: "+filename);
+				
+				FileInputStream imgfile = new FileInputStream(filename);
+
+				try {
+					byte[] buf = new byte[10240];
+					int len;
+					while ((len = imgfile.read(buf)) > 0) {
+						out.write(buf,0, len);
+					}				
+					
+					imgfile.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				out.flush();
+				out.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			int code = connector.getResponseCode();
+			System.out.println("Response is: "+code);
 			connector.disconnect();
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
@@ -34,5 +68,15 @@ public class riseye {
 	public static void main(String[] argv)
 	{
 		System.out.println("Hello world!");
+		int loop = 0;
+		while( loop++ < 100 )
+		{
+			(new riseye()).PostIt();
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
