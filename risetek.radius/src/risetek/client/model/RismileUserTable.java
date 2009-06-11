b79a26a5b5b6a5eed9de7f1f4cb35622aada0316
ychun.w <ychun.w@gmail.com>
2009-06-11 09:37:16 +0000

    用绿色背景表明用户在线的数据准备

--- risetek.radius/src/risetek/client/model/RismileUserTable.java
+++ risetek.radius/src/risetek/client/model/RismileUserTable.java
@@ -16,16 +16,19 @@ public class RismileUserTable extends RismileTable {
 		
 		setSum(Integer.parseInt(sum));
 		NodeList users = entryElement.getElementsByTagName("rowid");
-		String data[][] = new String[users.getLength()][6];
+		String data[][] = new String[users.getLength()][7];
 		int j = 0;
-		for(;j<users.getLength();j++ ) {
+		for(;j<users.getLength();j++ )
+		{
 			Element logElement = (Element)users.item(j);
-			data[j][0] = logElement.getFirstChild().getNodeValue();//getElementText( logElement, "rowid" );
+			data[j][0] = logElement.getFirstChild().getNodeValue();
 			data[j][1] = XMLDataParse.getElementText( logElement, "IMSI" );
 			data[j][2] = XMLDataParse.getElementText( logElement, "USER" );
-			data[j][3] = "******";
+			data[j][3] = "****";
 			data[j][4] = IPConvert.longString2IPString(XMLDataParse.getElementText( logElement, "ADDRESS" ));
-			data[j][5] = XMLDataParse.getElementText( logElement, "STATUS" ).equals("1") ? "在线":"";;
+			data[j][5] = XMLDataParse.getElementText( logElement, "NOTE" );
+//			data[j][6] = XMLDataParse.getElementText( logElement, "STATUS" ).equals("1") ? "在线":"";
+			data[j][6] = XMLDataParse.getElementText( logElement, "STATUS" );
 		}
 		setData(data);
 	}
