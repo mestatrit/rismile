394d507bdd6a3cdc2a0db63a3361e8253e97f25b
ychun.w <ychun.w@gmail.com>
2009-07-19 10:17:27 +0000

    用simplePanel

--- rismile.scada/src/com/risetek/scada/client/view/mapsView.java
+++ rismile.scada/src/com/risetek/scada/client/view/mapsView.java
@@ -2,17 +2,18 @@ package com.risetek.scada.client.view;
 
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 
 public class mapsView extends Composite {
 
-	private final VerticalPanel frame = new VerticalPanel();
-
+	private final SimplePanel frame = new SimplePanel();
+	private final MapWidget map = new MapWidget(); 
 	public mapsView() {
-		MapWidget map = new MapWidget();
 		map.setDraggable(true);
 	    map.setScrollWheelZoomEnabled(true);
 	    map.setInfoWindowEnabled(true);
+		//frame.setWidth("100%");
+		//frame.setHeight(Entry.SinkHeight);
 	    map.setSize("800px", "640px");
 	    frame.add(map);
 	    initWidget(frame);
