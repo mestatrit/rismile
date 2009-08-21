c883644b122ec0f637e70a783e12bdafd2a12f4f
ychun.w <ychun.w@gmail.com>
2009-08-21 09:52:26 +0000

    更改图片获取的方式

--- rismile.riseye/src/com/risetek/scada/client/view/cameraView.java
+++ rismile.riseye/src/com/risetek/scada/client/view/cameraView.java
@@ -1,6 +1,8 @@
 package com.risetek.scada.client.view;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ErrorEvent;
+import com.google.gwt.event.dom.client.ErrorHandler;
 import com.google.gwt.event.dom.client.LoadEvent;
 import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.user.client.Timer;
@@ -27,11 +29,23 @@ public class cameraView extends Composite {
 		initWidget(frame);
 		frame.setWidth("100%");
 
+		photo.addLoadHandler(new LoadHandler(){
+			public void onLoad(LoadEvent event) {
+				MessageConsole.setText("图片得到 ");
+				//hbTimer.schedule(1000);
+				hbTimer.run();
+			}});
+		
+		photo.addErrorHandler(new ErrorHandler(){
+
+			public void onError(ErrorEvent event) {
+				MessageConsole.setText("图片未得到 ");
+				hbTimer.run();
+			}
+			
+		});
 		//photo.setUrl("/scada/camera");
 		//hbTimer.run();
-	}
-
-	public void onshow(){
 		hbTimer = new Timer() {
 			public void run() {
 				String imgname = "/scada/camera?id="+System.currentTimeMillis();
@@ -39,13 +53,12 @@ public class cameraView extends Composite {
 				GWT.log("获取图片: " + imgname ,null);
 				photo.setUrl(imgname);
 				//photo.prefetch(url);
-				photo.addLoadHandler(new LoadHandler(){
-					public void onLoad(LoadEvent event) {
-						MessageConsole.setText("图片得到 ");
-					}});
-				hbTimer.schedule(1000);
+				//hbTimer.schedule(3000);
 			}
 		};
+	}
+
+	public void onshow(){
 		hbTimer.run();
 	}
 
