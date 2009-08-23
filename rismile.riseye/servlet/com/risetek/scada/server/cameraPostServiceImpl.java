921490a2962dc520b3c1b447d760db5abbe597a3
ychun.w <ychun.w@gmail.com>
2009-08-23 14:50:30 +0000

    根据请求的长度来获取传输上来的数据。

--- rismile.riseye/servlet/com/risetek/scada/server/cameraPostServiceImpl.java
+++ rismile.riseye/servlet/com/risetek/scada/server/cameraPostServiceImpl.java
@@ -1,10 +1,7 @@
 package com.risetek.scada.server;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,85 +16,16 @@ public class cameraPostServiceImpl extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		GWT.log("camera", null);
 		int ContentLength = req.getContentLength();
-		GWT.log("content is: "+ContentLength, null);
-		byte[] cachebuf = null;
-		
+		GWT.log("Posted picture content is: "+ContentLength, null);
+		byte[] cachebuf = new byte[ContentLength];
 		InputStream imgData =  req.getInputStream();
 		try {
-			byte[] buf = new byte[10240];
-			int len;
-			while ((len = imgData.read(buf)) > 0) {
-				int oldlen;
-				if(cachebuf == null )
-					oldlen = 0;
-				else
-					oldlen = cachebuf.length;
-				
-				
-				byte[] newbuf = new byte[oldlen + len];
-				if( oldlen > 0)
-					System.arraycopy(cachebuf, 0, newbuf, 0, oldlen);
-				
-				System.arraycopy(buf, 0, newbuf, oldlen, len);
-				
-				cachebuf = newbuf;
-			}				
-			
+			imgData.read(cachebuf);
 			imgData.close();
+			ImageCache.imageCache.putImage(cachebuf);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		
-		
-		ImageCache.imageCache.putImage(cachebuf);
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		GWT.log("camera", null);
-		byte[] img = (byte[])req.getAttribute("img");
-		ImageCache.imageCache.putImage(img);
-	
-		byte[] cachebuf = null;
-		
-		// http://forums.smartclient.com/showthread.php?t=5258
-		try {
-			FileInputStream imgfile = new FileInputStream("image/p2.jpg");
-
-			try {
-				byte[] buf = new byte[10240];
-				int len;
-				while ((len = imgfile.read(buf)) > 0) {
-					int oldlen;
-					if(cachebuf == null )
-						oldlen = 0;
-					else
-						oldlen = cachebuf.length;
-					
-					
-					byte[] newbuf = new byte[oldlen + len];
-					if( oldlen > 0)
-						System.arraycopy(cachebuf, 0, newbuf, 0, oldlen);
-					
-					System.arraycopy(buf, 0, newbuf, oldlen, len);
-					
-					cachebuf = newbuf;
-				}				
-				
-				imgfile.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		ImageCache.imageCache.putImage(cachebuf);	
-	
 	}
 }
