fe42489a4bbd8124382e5ff96ea9bf0a5095ca2c
ychun.w <ychun.w@gmail.com>
2009-08-23 14:48:54 +0000

    对cache不做具体的处理

--- rismile.riseye/servlet/com/risetek/scada/server/cameraQueryServiceImpl.java
+++ rismile.riseye/servlet/com/risetek/scada/server/cameraQueryServiceImpl.java
@@ -16,12 +16,11 @@ public class cameraQueryServiceImpl extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		GWT.log("output picture", null);
-		// TODO: no-cache
-		resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+		// 我们通过请求附带的时间戳来避免客户端的cache。
+		//resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("image/jpg");
 		ServletOutputStream out = resp.getOutputStream();
-
 		byte[] data = ImageCache.imageCache.getImage();
 		if( data != null )
 			out.write(data, 0, data.length);
