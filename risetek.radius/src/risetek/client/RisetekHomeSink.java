e4342f7f1ff54b0af7383691344eac206c4403bf
ychun.w <ychun.w@gmail.com>
2009-11-18 02:18:11 +0000

    增加tongfa标记

--- risetek.radius/src/risetek/client/RisetekHomeSink.java
+++ risetek.radius/src/risetek/client/RisetekHomeSink.java
@@ -2,6 +2,7 @@ package risetek.client;
 
 import risetek.client.control.ProduceHomeController;
 
+import com.risetek.rismile.client.Entry;
 import com.risetek.rismile.client.sink.Sink;
 
 
@@ -11,11 +12,19 @@ public class RisetekHomeSink extends Sink {
 	ProduceHomeController control = new ProduceHomeController();
 	
 	public static SinkInfo init() {
+		if(Entry.OEMFlag == Entry.OEM.risetek )
 		return new SinkInfo(Tag, "欢迎", "欢迎使用成都中联信通科技有限公司产品") {
 			public Sink createInstance() {
 				return new RisetekHomeSink();
 			}
 		};
+		else
+			return new SinkInfo(Tag, "欢迎", "欢迎使用本公司产品") {
+			public Sink createInstance() {
+				return new RisetekHomeSink();
+			}
+		};
+			
 	}
 	public RisetekHomeSink(){
 		initWidget(control.view);
