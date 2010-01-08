58d22212a4e7be370c86a226a72d84a5b5d037f6
ychun.w <ychun.w@gmail.com>
2010-01-08 04:32:24 +0000

    未使用

--- rismile.riseye/src/com/risetek/scada/client/remote/ImgService.java
+++
@@ -1,7 +0,0 @@
-package com.risetek.scada.client.remote;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ImgService {
-	public void getImg(String s, AsyncCallback<String> callback);
-}
