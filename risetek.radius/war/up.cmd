d13c983710ab59ec7e597cf8ff57147c1d00dd47
ychun.w <ychun.w@gmail.com>
2010-05-14 16:01:56 +0000

    新结构版本

--- risetek.radius/war/up.cmd
+++ risetek.radius/war/up.cmd
@@ -1,2 +1,3 @@
 for %%c in (./rismile/*.*) do tftp -i %1 PUT ./rismile/%%c /webs/%%c
 for %%c in (./rismile/images/*.*) do tftp -i %1 PUT ./rismile/images/%%c /webs/images/%%c
+for %%c in (./rismile/images/ie6/*.*) do tftp -i %1 PUT ./rismile/images/ie6/%%c /webs/images/ie6/%%c
