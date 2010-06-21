099c3b2ecf041441f394254a06175338fddb98b8
googling.wang <googling.wang@gmail.com>
2010-06-21 16:50:52 +0000

    修改

--- risetek.keke/src/com/risetek/keke/client/ticker/PromotionTicker.java
+++ risetek.keke/src/com/risetek/keke/client/ticker/PromotionTicker.java
@@ -1,9 +1,14 @@
 package com.risetek.keke.client.ticker;
 
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.risetek.keke.server.node.Node;
 
-public class PromotionTicker {
+public class PromotionTicker extends Composite {
+	
+	HTMLPanel panel = new HTMLPanel("");
+	
 	public PromotionTicker(Node node) {
-		
+		initWidget(panel);
 	}
 }
