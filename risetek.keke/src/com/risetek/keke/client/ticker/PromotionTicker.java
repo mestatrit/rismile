1c9ffb9b930faf1114ac49b3ab6a0cdbedb836db
googling.wang <googling.wang@gmail.com>
2010-06-23 14:29:28 +0000

    试图实现一种工厂模式

--- risetek.keke/src/com/risetek/keke/client/ticker/PromotionTicker.java
+++ risetek.keke/src/com/risetek/keke/client/ticker/PromotionTicker.java
@@ -1,13 +1,10 @@
 package com.risetek.keke.client.ticker;
 
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.risetek.keke.server.node.Node;
 
-public class PromotionTicker extends Composite {
+public class PromotionTicker extends Ticker {
 	
-	HTMLPanel panel = new HTMLPanel("");
-	
+
 	public PromotionTicker(Node node) {
 		initWidget(panel);
 	}
---
+++ risetek.keke/src/com/risetek/keke/client/ticker/Ticker.java
@@ -0,0 +1,11 @@
+package com.risetek.keke.client.ticker;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+
+public class Ticker extends Composite {	
+	HTMLPanel panel = new HTMLPanel("");
+	public Ticker() {
+		
+	}
+}
