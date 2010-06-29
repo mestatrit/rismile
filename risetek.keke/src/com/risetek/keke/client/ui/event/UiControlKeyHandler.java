9644148c079d0922d38c5406f049ce42a6e80f11
googling.wang <googling.wang@gmail.com>
2010-06-29 13:55:34 +0000

    过时的设计

--- risetek.keke/src/com/risetek/keke/client/ui/event/UiControlKeyHandler.java
+++
@@ -1,7 +0,0 @@
-package com.risetek.keke.client.ui.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface UiControlKeyHandler extends EventHandler {
-	void onUiControlKeyPressed(UiControlKeyEvent event);
-}
