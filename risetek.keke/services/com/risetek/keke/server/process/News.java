6f44d6afef194a0408d1820bb89b8b8e5a294a2f
ychun.w <ychun.w@gmail.com>
2010-07-06 10:21:52 +0000

    整理代码

--- risetek.keke/services/com/risetek/keke/server/process/News.java
+++ risetek.keke/services/com/risetek/keke/server/process/News.java
@@ -1,9 +1,7 @@
 package com.risetek.keke.server.process;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,14 +12,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
-
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import com.google.gwt.core.client.GWT;
 import com.risetek.keke.client.sticklet.Sticklets;
 
 public class News {
