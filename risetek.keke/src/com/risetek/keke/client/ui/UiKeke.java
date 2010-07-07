2912b1d4c021dc8f11f96ced543d19e624bab15c
googling.wang <googling.wang@gmail.com>
2010-07-07 15:39:33 +0000

    整理一下界面

--- risetek.keke/src/com/risetek/keke/client/ui/UiKeke.java
+++ risetek.keke/src/com/risetek/keke/client/ui/UiKeke.java
@@ -3,5 +3,5 @@ package com.risetek.keke.client.ui;
 public interface UiKeke {
 	public static int maxKeke		= 6;
 	public static int hiKeke		= 1;		// begin at 0;
-	public static int kekeHeight	= 60;
+	public static int kekeHeight	= 65;
 }
