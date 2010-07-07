20a2cd98dcda176027459401d1525941f787e0cd
ychun.w <ychun.w@gmail.com>
2010-07-07 06:10:05 +0000

    修改 Context 为主要参数前的标本

--- risetek.radius/war/up.cmd
+++
@@ -1,3 +0,0 @@
-for %%c in (./rismile/*.*) do tftp -i %1 PUT ./rismile/%%c /webs/%%c
-for %%c in (./rismile/images/*.*) do tftp -i %1 PUT ./rismile/images/%%c /webs/images/%%c
-for %%c in (./rismile/images/ie6/*.*) do tftp -i %1 PUT ./rismile/images/ie6/%%c /webs/images/ie6/%%c
