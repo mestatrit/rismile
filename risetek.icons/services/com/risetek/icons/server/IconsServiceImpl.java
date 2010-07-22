0344c5908b10c09b79c05d431d5443477194f3f2
googling.wang <googling.wang@gmail.com>
2010-07-22 17:17:33 +0000

    提供多个上传的能力

--- risetek.icons/services/com/risetek/icons/server/IconsServiceImpl.java
+++ risetek.icons/services/com/risetek/icons/server/IconsServiceImpl.java
@@ -2,6 +2,8 @@ package com.risetek.icons.server;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -44,21 +46,26 @@ public class IconsServiceImpl extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
-		String request = req.getRequestURI().substring(7);	// Skip /icons/
-		log.info("upload:"+request);
-
+		resp.setCharacterEncoding("UTF-8");
 		ServletFileUpload upload = new ServletFileUpload(); 
 		try {
 			FileItemIterator iterator = upload.getItemIterator(req);
 			while (iterator.hasNext()) {
 				FileItemStream item = iterator.next();
+				String fileName = item.getName().replace("\\","/");
+				fileName = fileName.replace("/icons/", "");
+				fileName = fileName.substring(fileName.lastIndexOf("/")+1);
 				InputStream stream = item.openStream();
 				byte[] remoteImg = IOUtils.toByteArray(stream);
-				Icon icon = new Icon(request, remoteImg);
+				Icon icon = new Icon(fileName, remoteImg);
 				icon.save();
 			}
 		} catch (FileUploadException e1) {
 			log.log(Level.WARNING, e1.toString());
 		}
+		PrintWriter pw= resp.getWriter();
+		pw.write("上传成功");
+		pw.flush();
+		pw.close();
 	}
 }
--- risetek.icons/src/com/risetek/icons/client/ui/Icons.java
+++ risetek.icons/src/com/risetek/icons/client/ui/Icons.java
@@ -1,6 +1,8 @@
 package com.risetek.icons.client.ui;
 
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
@@ -21,56 +23,73 @@ import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
 import com.google.gwt.user.client.ui.FormPanel.SubmitHandler;
 
 public class Icons extends Composite {
-
-	private final FileUpload fileUpload = new FileUpload();
-	private Button upload = new Button("开始上传文件");
-	private final FormPanel formPanel = new FormPanel();
 	private final VerticalPanel vpOuter = new VerticalPanel();
-	private final VerticalPanel vp = new VerticalPanel();
-	private final HTML status = new HTML(" ");
 
 	public Icons() {
-		fileUpload.setName("file1");
-		//upload.setWidth("210px");
-		//upload.setHeight("40px");
-		upload.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				if (fileUpload.getFilename().length() != 0) {
-					formPanel.setAction("/icons/"+fileUpload.getFilename());
-					formPanel.submit();
-				} else
-					Window.alert("请选择上传的文件!");
-			}
-		});
-		formPanel.setMethod(FormPanel.METHOD_POST);
-		formPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
-
-		formPanel.add(fileUpload);
-		formPanel.addSubmitHandler(new SubmitHandler() {
-			public void onSubmit(SubmitEvent event) {
-				status.setHTML("开始上传...");
-			}
-		});
-
-		formPanel.addSubmitCompleteHandler(new SubmitCompleteHandler() {
-			public void onSubmitComplete(SubmitCompleteEvent event) {
-				status.setHTML(event.getResults());
-			}
-		});
-		vp.setBorderWidth(1);
-		vp.add(formPanel);
-		vp.add(upload);
-		vp.add(status);
-		// vp.setSize("80%", "100%");
-		vp.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
-		vp.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		UploadComposite upload1 = new UploadComposite("upload1");
+		UploadComposite upload2 = new UploadComposite("upload2");
+		UploadComposite upload3 = new UploadComposite("upload3");
+		UploadComposite upload4 = new UploadComposite("upload4");
 
 		vpOuter.setBorderWidth(1);
 		vpOuter.setSize("100%", "100%");
 		vpOuter.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
 		vpOuter.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-		vpOuter.add(vp);
+		vpOuter.add(upload1);
+		vpOuter.add(upload2);
+		vpOuter.add(upload3);
+		vpOuter.add(upload4);
+
 		initWidget(vpOuter);
 	}
 
+	private class UploadComposite extends Composite {
+		private final FileUpload fileUpload = new FileUpload();
+		private final FormPanel formPanel = new FormPanel();
+		private final VerticalPanel vp = new VerticalPanel();
+		private final HTML status = new HTML("文件传输状态");
+
+		public UploadComposite(String name) {
+			fileUpload.setName("name");
+
+			fileUpload.addChangeHandler(new ChangeHandler() {
+
+				@Override
+				public void onChange(ChangeEvent event) {
+
+					if (fileUpload.getFilename().length() != 0) {
+						formPanel.setAction("/icons/"
+								+ fileUpload.getFilename());
+						formPanel.submit();
+					} else {
+						status.setTitle("未选择上传的文件!");
+					}
+				}
+			});
+
+			formPanel.setMethod(FormPanel.METHOD_POST);
+			formPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
+
+			formPanel.add(fileUpload);
+			formPanel.addSubmitHandler(new SubmitHandler() {
+				public void onSubmit(SubmitEvent event) {
+					status.setHTML("开始上传...");
+				}
+			});
+
+			formPanel.addSubmitCompleteHandler(new SubmitCompleteHandler() {
+				public void onSubmitComplete(SubmitCompleteEvent event) {
+					status.setHTML(event.getResults());
+				}
+			});
+			vp.setBorderWidth(1);
+			vp.add(formPanel);
+			vp.add(status);
+			// vp.setSize("80%", "100%");
+			vp.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+			vp.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+
+			initWidget(vp);
+		}
+	}
 }
